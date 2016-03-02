ae6444febd60a408a23c632895d0d8003178db92
Aaron Kenny <aaronmkenny@gmail.com>
2016-03-02 15:16:52 -0800

    Modify package.json

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -9,29 +9,6 @@ return!0}function Q(a,b,d,e){if(m.acceptData(a)){var f,g,h=m.expando,i=a.nodeTyp
   // Write all your custom JavaScript here
     
     
-    function copyText(text) {
-        var copyElement = document.createElement('input');
-        copyElement.setAttribute('type', 'text');
-        copyElement.setAttribute('value', text);
-        copyElement = document.body.appendChild(copyElement);
-        copyElement.select();
-        try {
-            if(!document.execCommand('copy')) throw 'Not allowed.';
-        } catch(e) {
-            copyElement.remove();
-            console.log("document.execCommand('copy'); is not supported");
-            prompt('Copy the text below. (ctrl c, enter)', text);
-        } finally {
-            if (typeof e == 'undefined') {
-                copyElement.remove();
-            }
-        }
-    }
-    
-    $('#copyButton').click(function(){
-        var textResults = $('.results .outerDiv').text();
-        copyText(textResults);
-    });
   
 })();
 
